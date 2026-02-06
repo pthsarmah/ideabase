538e36237bae0b51ffa98f3ab2c58e17bfdf8bab
Parthajeet-Sarmah <coderparth32768@gmail.com>
2026-02-06 19:13:03 +0530

    added edit and delete commands, updated readme

--- README.md
+++ README.md
@@ -17,16 +17,17 @@ Usage: idb [options] [command]
 source control for your brilliant ideas!!
 
 Options:
-  -V, --version                                  output the version number
-  -h, --help                                     display help for command
+  -V, --version                 output the version number
+  -h, --help                    display help for command
 
 Commands:
-  init                                           Init a global repo of ideas
-  list [options]                                 List ideas
-  add [options] <idea>                           Add an idea
-  edit <hash> <-t, --title> <-d, --description>  Edit an ideas (NOT COMPLETE)
-  mark <hash>                                    Mark idea as done/complete
-  unmark <hash>                                  Unmark idea from done/complete
-  rm                                             Remove an idea (NOT COMPLETE)
-  help [command]                                 display help for command
+  init                          init a global repo of ideas
+  list [options]                list ideas
+  add [options] <idea>          add an idea
+  edit [options] <hash>         edit an idea
+  mark <hash>                   mark idea as done/complete
+  unmark <hash>                 unmark idea from done/complete
+  rm <hash>                     remove idea
+  delete <hash>                 remove idea
+  help [command]                display help for command
 ```
--- idb.ts
+++ idb.ts
@@ -9,11 +9,11 @@ import { showHashesWithTitles, showIdeaViewer } from './tui';
 program
 	.name("idb")
 	.description("source control for your brilliant ideas!!")
-	.version("0.0.1");
+	.version("1.0.0");
 
 program
 	.command('init')
-	.description('Init a global repo of ideas')
+	.description('init a global repo of ideas')
 	.action(async () => {
 		fs.mkdir(`${tmpdir()}/.idb`, { recursive: true }, (err) => {
 			if (err)
@@ -24,9 +24,9 @@ program
 
 program
 	.command('list')
-	.description('List ideas')
+	.description('list ideas')
 	.option('-m, --marked', 'list only marked ideas')
-	.option('-a, --all', 'list all ideas')
+	.option('-u, --unmarked', 'list only unmarked ideas')
 	.option('-H, --hash', 'list only hashes with titles')
 	.action(async (options) => {
 
@@ -40,8 +40,15 @@ program
 
 		var ideasJson: { hash: string, idea: string, description: string, marked: boolean }[] = await Bun.file(ideasFile).json();
 
+		if (ideasJson.length <= 0) {
+			console.error("No ideas available");
+			return;
+		}
+
 		if (options.marked) {
 			ideasJson = ideasJson.filter(i => i.marked);
+		} else if (options.unmarked) {
+			ideasJson = ideasJson.filter(i => !i.marked);
 		}
 
 		if (options.hash) {
@@ -55,12 +62,12 @@ program
 //add ideas
 program
 	.command('add')
-	.description('Add an idea')
+	.description('add an idea')
 	.argument('<idea>', 'your brilliant idea')
 	.option('-d, --description <string>', 'add an idea description')
 	.action(async (idea, options) => {
 		if (!idea) { console.error("No idea provided"); return; }
-		const description = options.description;
+		const description = options.description.trim();
 		const body = `idea: ${idea}\ndescription: ${description ?? `no description`}`;
 		const hash = new Bun.CryptoHasher("sha256").update(body).digest("hex");
 
@@ -92,18 +99,43 @@ program
 //edit ideas
 program
 	.command('edit')
-	.description('Edit an ideas')
+	.description('edit an idea')
 	.argument('<hash>', 'hash of your idea')
-	.argument('-t, --title', 'new title')
-	.argument('-d, --description', 'new description')
-	.action(() => {
+	.option('-t, --title <string>', 'new title')
+	.option('-d, --description <string>', 'new description')
+	.action(async (hash, options) => {
+		const ideasFile = `${tmpdir()}/.idb/ideas.json`;
 
+		if (!existsSync(`${tmpdir()}/.idb`)) {
+			console.error(".idb directory does not exist. Run 'init'");
+			return;
+		}
+
+		const ideaFileExists = await Bun.file(ideasFile).exists();
+
+		if (!ideaFileExists) {
+			console.error("ideas.json file does not exist. Run 'init'");
+		}
+
+		var ideasJson: { hash: string, idea: string, description: string, marked: boolean }[] = await Bun.file(ideasFile).json();
+
+		const title = options.title.trim() ?? null;
+		const description = options.description.trim() ?? null;
+
+		if (hash?.length === 7)
+			ideasJson = ideasJson.map(i => (i.hash.substring(0, 7) === hash ? { ...i, idea: title ?? i.idea, description: description ?? i.description } : i));
+		else if (hash?.length === 64)
+			ideasJson = ideasJson.map(i => (i.hash === hash ? { ...i, idea: title ?? i.idea, description: description ?? i.description } : i));
+		else
+			console.error("Invalid hash provided!");
+
+		await Bun.write(ideasFile, JSON.stringify(ideasJson, null, 2));
 	});
 
 //mark idea as done
 program
 	.command('mark')
-	.description('Mark idea as done/complete')
+	.description('mark idea as done/complete')
 	.argument('<hash>', 'hash of your idea')
 	.action(async (hash) => {
 		const ideasFile = `${tmpdir()}/.idb/ideas.json`;
@@ -133,7 +165,7 @@ program
 
 program
 	.command('unmark')
-	.description('Unmark idea from done/complete')
+	.description('unmark idea from done/complete')
 	.argument('<hash>', 'hash of your idea')
 	.action(async (hash) => {
 		const ideasFile = `${tmpdir()}/.idb/ideas.json`;
@@ -164,11 +196,66 @@ program
 //remove idea
 program
 	.command('rm')
-	.command('remove')
-	.description('Remove idea')
+	.description('remove idea')
 	.argument('<hash>', 'hash of your idea')
-	.action(() => {
+	.action(async (hash) => {
+		const ideasFile = `${tmpdir()}/.idb/ideas.json`;
 
+		if (!existsSync(`${tmpdir()}/.idb`)) {
+			console.error(".idb directory does not exist. Run 'init'");
+			return;
+		}
+
+		const ideaFileExists = await Bun.file(ideasFile).exists();
+
+		if (!ideaFileExists) {
+			console.error("ideas.json file does not exist. Run 'init'");
+		}
+
+		var ideasJson: { hash: string, idea: string, description: string, marked: boolean }[] = await Bun.file(ideasFile).json();
+
+		if (hash?.length === 7)
+			ideasJson = ideasJson.filter(i => i.hash.substring(0, 7) !== hash);
+		else if (hash?.length === 64)
+			ideasJson = ideasJson.filter(i => i.hash !== hash);
+		else
+			console.error("Invalid hash provided!");
+
+		console.log(`Removed idea hashed ${hash}`);
+
+		await Bun.write(ideasFile, JSON.stringify(ideasJson, null, 2));
+	});
+
+program
+	.command('delete')
+	.description('remove idea')
+	.argument('<hash>', 'hash of your idea')
+	.action(async (hash) => {
+		const ideasFile = `${tmpdir()}/.idb/ideas.json`;
+
+		if (!existsSync(`${tmpdir()}/.idb`)) {
+			console.error(".idb directory does not exist. Run 'init'");
+			return;
+		}
+
+		const ideaFileExists = await Bun.file(ideasFile).exists();
+
+		if (!ideaFileExists) {
+			console.error("ideas.json file does not exist. Run 'init'");
+		}
+
+		var ideasJson: { hash: string, idea: string, description: string, marked: boolean }[] = await Bun.file(ideasFile).json();
+
+		if (hash?.length === 7)
+			ideasJson = ideasJson.filter(i => i.hash.substring(0, 7) !== hash);
+		else if (hash?.length === 64)
+			ideasJson = ideasJson.filter(i => i.hash !== hash);
+		else
+			console.error("Invalid hash provided!");
+
+		console.log(`Removed idea hashed ${hash}`);
+
+		await Bun.write(ideasFile, JSON.stringify(ideasJson, null, 2));
 	});
 
 program.parse(process.argv);
